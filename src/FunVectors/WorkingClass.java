0c8d28ecfd3477fa30a4bda3f63443a22472abbe
devianvii <ilya.plakitin@gmail.com>
2014-02-20 13:13:33 +0400

    Up Working

--- src/FunVectors/WorkingClass.java
+++ src/FunVectors/WorkingClass.java
@@ -3,8 +3,8 @@ package FunVectors;
 public class WorkingClass {
 
     public static void main(String[] args) {
-        Vector c1 = new Vector(3, 4, 4);
-        Vector c2 = new Vector(5, 3, 3);
+        Vector c1 = new Vector(0, 0, 0, 3, 7, 4);
+        Vector c2 = new Vector(0, 2, 0, 4, 0, -2);
         System.out.println("Имеем вектора:\r\nВектор 1:\r\n" + c1 + " \r\nВектор 2:\r\n" + c2);
         System.out.println("\r\nВычтем вектора 1 и 2, получим вектор:" + c1.Вычесть(c2));
         System.out.println("Модуль вектора 2: " + c2.Модуль());
