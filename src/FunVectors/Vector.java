3a64d83c630bed57a30a6e7f726a7195fc0b37ea
devianvii <ilya.plakitin@gmail.com>
2014-02-22 13:23:41 +0400

    UP
    исправления,не до конца

--- src/FunVectors/Vector.java
+++ src/FunVectors/Vector.java
@@ -2,69 +2,44 @@ package FunVectors;
 
 public class Vector {
 
-    private final int x1, x2, x3, y1, y2, y3;
+    private final int a, b, c;
     private Vector vector;
 
-    public Vector(int x1, int x2, int x3, int y1, int y2, int y3) { // (x1,x2,x3) (y1,y2,y3)
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
-        this.y1 = y1;
-        this.y2 = y2;
-        this.y3 = y3;
+    public Vector(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
 
     }
 
-    public Vector(int y1, int y2, int y3) {
-        this.x1 = 0;
-        this.x2 = 0;
-        this.x3 = 0;
-        this.y1 = y1;
-        this.y2 = y2;
-        this.y3 = y3;
+    public Vector Add(Vector other) {
+
+        return new Vector(a + other.a, b + other.b, c + other.c);
     }
 
-    public Vector Сложить(Vector Z) {
-        Z = Z.ПереносВНачало();
-        this.ПереносВНачало(); // ???????
-        return new Vector(y1 + Z.y1, y2 + Z.y2, y3 + Z.y3);
+    public Vector subtract(Vector other) {
+
+        return new Vector(a - other.a, b - other.b, c - other.c);
     }
 
-    public Vector Вычесть(Vector Z) {
-        Z = Z.ПереносВНачало();
-        this.ПереносВНачало(); // ???????
-        return new Vector(y1 - Z.y1, y2 - Z.y2, y3 - Z.y3);
+    public double Module() {
+        return Math.pow(a * a + b * b + c * c, 0.5);
     }
 
-    public double Модуль() {
-        this.ПереносВНачало(); // ???????
-        return Math.pow(this.y1 * this.y1 + this.y2 * this.y2 + this.y3 * this.y3, 0.5);
+    public double СкалярноеПроизведение(Vector other) {
+
+        return this.a * other.a + this.b * other.b + this.c * other.c;
     }
 
-    public double СкалярноеПроизведение(Vector Z) {
-        Z = Z.ПереносВНачало();
-        this.ПереносВНачало(); // ???????
-        return this.y1 * Z.y1 + this.y2 * Z.y2 + this.y3 * Z.y3;
-    }
+    public Vector ВекторноеПроизведение(Vector other) {
 
-    public Vector ВекторноеПроизведение(Vector Z) {
-        Z = Z.ПереносВНачало();
-        this.ПереносВНачало(); // ???????
-        return new Vector(this.y2 * Z.y3 - this.y3 * Z.y2, this.y1 * Z.y3 - this.y3 * Z.y1, this.y1 * Z.y2 - this.y2 * Z.y1);
-    }
-
-    private Vector ПереносВНачало() {  //  тернарный оператор не работает
-        if ((this.x1 != 0) || (this.x2 != 0) || (this.x3 != 0)) {
-            return new Vector(this.y1 - this.x1, this.y2 - this.x2, this.y3 - this.x3);
-        } else {
-            return new Vector(this.x1, this.x2, this.x3, this.y1, this.y2, this.y3); //обязательно ли возвращать новый вектор?
-        }
+        return new Vector(this.b * other.c - this.c * other.b, this.a * other.c - this.c * other.a, this.a * other.b - this.b * other.a);
     }
 
     @Override
     public String toString() {
 
-        return String.valueOf(x1 != 0 || x2 != 0 || x3 != 0 ? " Координаты начала: (" + x1 + ";" + x2 + ";" + x3 + ")\r\n  Координаты конца:" : "") + "(" + y1 + ";" + y2 + ";" + y3 + ")";
+        return String.valueOf("(" + a + ";" + b + ";" + c + ")");
     }
 
 }
--- src/FunVectors/WorkingClass.java
+++ src/FunVectors/WorkingClass.java
@@ -4,14 +4,10 @@ public class WorkingClass {
 
     public static void main(String[] args) {
 
-        Vector c1 = new Vector(0, 0, 0, 3, 7, 4);
-        Vector c2 = new Vector(0, 2, 0, 4, 0, -2);
+        Vector c1 = new Vector(1, 1, 1);
 
-        System.out.println("Имеем вектора:\r\nВектор 1:\r\n" + c1 + " \r\nВектор 2:\r\n" + c2);
-        System.out.println("\r\nВычтем вектора 1 и 2, получим вектор:" + c1.Вычесть(c2));
-        System.out.println("Модуль вектора 2: " + c2.Модуль());
-        System.out.println("Скалярное произведение векторов 1 и 2: " + c1.СкалярноеПроизведение(c2));
-        System.out.println("Векторное произведение векторов 1 и 2: " + c1.ВекторноеПроизведение(c2));
+        System.out.println("Модуль вектора 2: " + c1.Module());
+
     }
 
 }
